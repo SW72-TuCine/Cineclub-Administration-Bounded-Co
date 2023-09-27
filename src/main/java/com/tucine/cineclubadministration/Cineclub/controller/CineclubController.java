094c14aae080532e22fd043443e5438e0c7c7e70
Axel Fiestas <josue123452002@gmail.com>
2023-09-27 01:07:31 -0500

    feat: Cineclub Controller implemented with methods

---
+++ src/main/java/com/tucine/cineclubadministration/Cineclub/controller/CineclubController.java
@@ -0,0 +1,66 @@
+package com.tucine.cineclubadministration.Cineclub.controller;
+
+import com.tucine.cineclubadministration.Cineclub.dto.normal.CineclubDto;
+import com.tucine.cineclubadministration.Cineclub.dto.receive.CineclubReceiveDto;
+import com.tucine.cineclubadministration.Cineclub.service.interf.CineclubService;
+import com.tucine.cineclubadministration.Film.dto.normal.FilmDto;
+import com.tucine.cineclubadministration.Film.model.Film;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/TuCine/v1/cineclub_administration")
+public class CineclubController {
+
+    @Autowired
+    private CineclubService cineclubService;
+
+    // URL: http://localhost:8080/api/TuCine/V1/cineclubs
+    // Method: GET
+    @Transactional(readOnly = true)
+    @GetMapping("/cineclubs")
+    public ResponseEntity<List<CineclubDto>> getAllCineclubs(){
+        return new ResponseEntity<>(cineclubService.getAllCineclubs(), org.springframework.http.HttpStatus.OK);
+    }
+
+    //URL: http://localhost:8080/api/TuCine/v1/cineclub_administration/cineclubs/
+    //Method: POST
+    @Transactional()
+    @PostMapping("/cineclubs")
+    public ResponseEntity<CineclubDto> createCineclub(CineclubReceiveDto cineclubReceiveDto){
+        return new ResponseEntity<>(cineclubService.createCineclub(cineclubReceiveDto), org.springframework.http.HttpStatus.CREATED);
+    }
+
+    //URL: http://localhost:8080/api/TuCine/v1/cineclub_administration/cineclubs/{cineclubId}
+    //Method: GET
+    @Transactional(readOnly = true)
+    @GetMapping("/cineclubs/{cineclubId}")
+    public ResponseEntity<CineclubDto> getCineclubById(Long cineclubId){
+        return new ResponseEntity<>(cineclubService.getCineclubById(cineclubId), org.springframework.http.HttpStatus.OK);
+    }
+
+    //URL: http://localhost:8080/api/TuCine/v1/cineclub_administration/cineclubs/{cineclubId}
+    //Method: GET
+    @Transactional(readOnly = true)
+    @GetMapping("/cineclubs/{cineclubId}")
+    public ResponseEntity<CineclubDto> getCineclubByName(String cineclubName){
+        return new ResponseEntity<>(cineclubService.getCineclubByName(cineclubName), org.springframework.http.HttpStatus.OK);
+    }
+
+    //URL: http://localhost:8080/api/TuCine/v1/cineclub_administration/cineclubs/{cineclubId}/films
+    //Method: GET
+    @Transactional()
+    @GetMapping("/cineclubs/{cineclubId}/films")
+    public ResponseEntity<List<FilmDto>> getAllMoviesByCineclubId(Long cineclubId){
+        return new ResponseEntity<>(cineclubService.getAllMoviesByCineclubId(cineclubId), org.springframework.http.HttpStatus.OK);
+    }
+
+
+}
