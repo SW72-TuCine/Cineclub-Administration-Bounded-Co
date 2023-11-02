276d98216ae51afd49b7990796b01420ffd46dbd
Axel Fiestas <josue123452002@gmail.com>
2023-11-02 01:49:13 -0500

    feat: issue with load balancer fixed

--- src/main/java/com/tucine/cineclubadministration/Cineclub/client/UserClient.java
+++ src/main/java/com/tucine/cineclubadministration/Cineclub/client/UserClient.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@FeignClient(name = "account-managment-service",url = "http://localhost:8082",path = "/api/TuCine/v1/account_management")
+@FeignClient(name = "account-managment-service",path = "/api/TuCine/v1/account_management")
 public interface UserClient {
     @RequestMapping("/users/verify/{userId}")
     boolean checkIfUserExist(@PathVariable("userId") Long userId) throws RuntimeException;
